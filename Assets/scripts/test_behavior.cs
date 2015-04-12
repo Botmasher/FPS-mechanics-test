ca9611bcb1394577d9626ebf6cbf960a66ec9015
Joshua R <languagejosh@gmail.com>
2015-04-12 19:52:54 -0400

    test Unity project upload

---
+++ Assets/scripts/test_behavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test_behavior : MonoBehaviour {
+
+	// Use this for initialization
+	private GameObject cube1;
+	void Start () {
+		cube1 = GameObject.Find ("Cube1");
+		cube1.transform.position = new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cube1.transform.Translate(Vector3.up * Time.deltaTime);
+	}
+}
