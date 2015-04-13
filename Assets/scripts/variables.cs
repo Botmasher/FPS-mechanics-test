e5ff6eefd11fb7deda8c78d4ea3969ff07d62d6d
Botmasher <languagejosh@hotmail.com>
2015-04-13 17:14:15 -0400

    Change tutorial test code scene 01

---
+++ Assets/scripts/variables.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variables : MonoBehaviour {
+	int myInt = 5;
+
+	void Start () {
+		Debug.Log (MultiplyByTwo (myInt));	
+	}
+
+	int MultiplyByTwo (int num) {
+		int returnVal;
+
+		returnVal = num * 2;
+		return returnVal;
+	}
+
+}
