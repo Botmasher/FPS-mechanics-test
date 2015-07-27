eee3cb509f3a3bf9e840de942c2dbeaba7b63da4
Botmasher <languagejosh@hotmail.com>
2015-07-27 17:10:31 -0400

    Test new  stats, with change and display methods in player1 stats manager

--- Assets/STRATEGY_TEST/StatsManager.cs
+++ Assets/STRATEGY_TEST/StatsManager.cs
@@ -3,5 +3,16 @@ using System.Collections;
 
 public class StatsManager : MonoBehaviour {
 
+	Stats.Statistics stats = new Stats.Statistics(1);
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			this.stats.Change (1,1);
+			this.stats.Display ();
+		}
+
+	}
 
 }
