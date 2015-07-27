04977b94b26ac3f441862426aa47c7bc7c8244de
Botmasher <languagejosh@hotmail.com>
2015-07-27 16:41:10 -0400

    Create strategy test stats and inventory item classes to be instantiated during gameplay

---
+++ Assets/STRATEGY_TEST/Inventory/Items.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ *	class for creating items:
+ *		- show up in player inventory
+ *		- built in world
+ *		- impact player stats
+ */
+public class Items : MonoBehaviour {
+
+	Sprite icon;							// player inventory UI image
+	GameObject mesh;						// world footprint and built visual
+	List<int> influences;					// player's influenced stats
+
+	// use player script?
+	Transform player;						// determine player possession for stats influence
+
+	public Items(Sprite icon, GameObject mesh, List<int> influences) {
+		this.icon = icon;
+		this.mesh = mesh;
+		this.influences = influences;
+	}
+
+	// influence the player's stats
+	void Stats () {
+		for (int i=0; i<influences.Count; i++) {
+			// select stat from instantiated player stats
+			// impact player stats script
+		}
+	}
+
+}
---
+++ Assets/STRATEGY_TEST/Stats.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// statistics for each player
+public class Stats : MonoBehaviour {
+
+	int playerID;
+	Dictionary<string,int> map = new Dictionary<string,int>();
+	List <string> names = new List<string>();
+
+	public Stats (int playerID) {
+		this.playerID = playerID;
+
+		// build list of attributes
+		this.names.Add ("tradition progress");
+		this.names.Add ("fear wonder");
+		this.names.Add ("authority liberty");
+
+		// map attributes to starting values
+		for (int x=0; x<names.Count; x++) {
+			this.map.Add(this.names[x], 0);
+		}
+	}
+
+	// use dictionary key or list name id to change stat value
+
+	public void ChangeStat (string statName, int valueAdd) {
+		this.map[statName] += valueAdd;
+	}
+
+	public void ChangeStat (int statID, int valueAdd) {
+		this.map [names [statID]] += valueAdd;
+	}
+
+}
