6f6ffe7934228cb3fc1c9a50778a9431f0111252
Botmasher <languagejosh@hotmail.com>
2015-07-27 19:48:28 -0400

    Build out strategy items class

--- Assets/STRATEGY_TEST/Inventory/Items.cs
+++ Assets/STRATEGY_TEST/Inventory/Items.cs
@@ -3,27 +3,51 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-/**
- *	class for creating items:
- *		- show up in player inventory
- *		- built in world
- *		- impact player stats
- */
 public class Items : MonoBehaviour {
 
+	public int statsCost;
+	public int peopleCost;
+	public int moneyCost;
+	[SerializeField]public List<Item> dependencies;
+	[SerializeField]public List<int> statsInfluences;
+	Sprite icon;
+	GameObject mesh;
+
+	// script for instantiated item object
+	private Item thisItem;
+
+
+	/**
+ 	 *	created items:
+ 	 *		- show up in player inventory
+ 	 *		- can be built in world
+ 	 *		- impact player stats
+ 	 *		- have dependencies
+ 	 *		- have costs
+ 	 */
 	public class Item {
 
 		Sprite icon;							// player inventory UI image
 		GameObject mesh;						// world footprint and built visual
 		List<int> influences;					// player's influenced stats
 
+		// costs
+		int peopleCost;
+		int moneyCost;
+		int statsCost;
+		List<Item> requirements;
+
 		// use player script?
 		Transform player;						// determine player possession for stats influence
 
-		public Item (Sprite icon, GameObject mesh, List<int> influences) {
+		public Item (Sprite icon, GameObject mesh, List<int> influences, int people, int money, int stats, List<Item> dependencies) {
 			this.icon = icon;
 			this.mesh = mesh;
 			this.influences = influences;
+			this.peopleCost = people;
+			this.moneyCost = money;
+			this.statsCost = stats;
+			this.requirements = dependencies;
 		}
 
 		// influence the player's stats
@@ -35,5 +59,11 @@ public class Items : MonoBehaviour {
 		}
 	
 	}
+
+
+	void Start () {
+		thisItem = new Item (icon, mesh, statsInfluences, peopleCost, moneyCost, statsCost, dependencies);
+	}
+
 
 }
--- Assets/STRATEGY_TEST/StatsManager.cs
+++ Assets/STRATEGY_TEST/StatsManager.cs
@@ -1,16 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class StatsManager : MonoBehaviour {
 
-	Stats.Statistics stats = new Stats.Statistics(1);
-	
-	// Update is called once per frame
+	public int playerID;					// player number to pass to stats object
+	Stats.Statistics stats;					// player attributes
+	List<int> updates = new List<int> ();	// stats to update this round
+	public bool isStatsChange = false;		// control flow for updating stats
+
+
+	void Start () {
+		stats = new Stats.Statistics (playerID);		// initialize new stats for this player
+	}
+
+
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.S)) {
-			this.stats.Change (1,1);
+		// update stats when time to
+		if (isStatsChange) {
+			for (int i = 0; i < updates.Count; i++) {
+				this.stats.Change (i, 1);
+			}
 			this.stats.Display ();
+			isStatsChange = false;
 		}
 
 	}
