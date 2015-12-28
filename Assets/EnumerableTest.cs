f548dfc5c6f2b21e2bd5c20da926384ef878c5b2
Botmasher <languagejosh@hotmail.com>
2015-12-28 13:40:56 -0500

    Test ienumerable with foreach

---
+++ Assets/EnumerableTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumerableTest : MonoBehaviour {
+
+	private bool runTestOnce;
+
+	void Start() {
+		runTestOnce = false;
+	}
+
+	void Update () {
+		if (!runTestOnce) {
+			foreach(int testVar in Test ()){
+				Debug.Log (testVar);
+			}
+			runTestOnce=true;
+		}
+	}
+
+	IEnumerable Test() {
+		for (int i=0; i<5; i++) {
+			yield return (i);
+		}
+		yield return (25);
+	}
+
+}
