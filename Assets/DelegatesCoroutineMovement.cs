3a9f62b253d57bfa12cdd2cdda986b62ff98f16c
Botmasher <languagejosh@hotmail.com>
2015-12-27 23:46:01 -0500

    Test raycast movement outside of Update with property, delegate and coroutine

---
+++ Assets/DelegatesCoroutineMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegatesCoroutineMovement : MonoBehaviour {
+
+	// this object's movement delegate
+	private delegate void MovementHandler (Vector3 position);
+	MovementHandler moveHandler;
+
+	// accessible property for target position
+	public Vector3 Pos {
+		get {
+			return pos;
+		} set {
+			pos = value;
+			StopCoroutine("ObjectMovementRoutine");
+			StartCoroutine ("ObjectMovementRoutine",pos);
+		}
+	} Vector3 pos; // private variable set by this property
+
+
+	// Overarching object movement coroutine
+	// uses movehandler to run specific funcitons at specific times
+	IEnumerator ObjectMovementRoutine (Vector3 targetPosition) {
+		moveHandler -= LogTarget;
+		moveHandler += LogTarget;
+		while (Vector3.Distance (transform.position, targetPosition) > 0.1f) {
+			moveHandler -= MoveToTarget;
+			moveHandler += MoveToTarget;
+			moveHandler (targetPosition);
+			yield return new WaitForEndOfFrame();
+		}
+		yield return null;
+	}
+
+	
+	/**
+	 * 	Movement functions for this object's movement delegate
+	 */
+
+	// Lerp to (or very close to) a target position over time
+	void MoveToTarget (Vector3 targetPosition) {
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime);
+	}
+
+	// Record the location of the target to the console
+	void LogTarget (Vector3 targetPosition) {
+		Debug.Log (""+targetPosition);
+	}
+	
+}
---
+++ Assets/DelegatesTest.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegatesTest : MonoBehaviour {
+
+	// variables for delegate to manipulate
+	public int ammoCount;
+	private bool isHealthy;
+	private bool isDead;
+
+	// delegates setup
+	private delegate void TrackInventory();
+	private TrackInventory trackingHandler;
+
+	// test switches for branching into delegate methods
+	public bool testInjury;
+	public bool testShooting;
+
+	/*
+	 *	Coroutine Movement using properties and delegates
+	 * (delegate and property in coroutine movement script)
+	 */
+	public DelegatesCoroutineMovement testMovement;
+
+
+	void Start () {
+		isHealthy = true;
+		isDead = false;
+	}
+
+	void Update () {
+		if (testInjury) {
+			trackingHandler += Injure;
+			testInjury = false;
+		} else if (testShooting) {
+			trackingHandler += ShootBullets;
+			testShooting = false;
+		} else {
+			// pass
+			//trackingHandler -= ShootBullets;
+			//trackingHandler -= Injure;
+		}
+
+		if (trackingHandler != null) {
+			trackingHandler();
+			trackingHandler -= ShootBullets;
+			trackingHandler -= Injure;
+		}
+
+		// grab a clicked object's position and use property coroutine movement script to lerp that object towards it
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast(ray, out hit);
+			if (hit.collider != null) {
+				testMovement.Pos = hit.point;
+			}
+		}
+	}
+
+	// Not running - using Input.GetMouseButtonDown above:
+	// http://answers.unity3d.com/questions/425478/onmousedown-not-firing.html
+//	void OnMouseDown () {
+//		Debug.Log ("DelegatesTest - OnMouseDown() is firing!");
+//		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//		RaycastHit hit;
+//		Physics.Raycast(ray, out hit);
+//		if (hit.collider.gameObject != null) {
+//			testMovement.Pos = hit.point+new Vector3(0f,0.5f,0f);
+//		}
+//	}
+
+
+	void Injure () {
+		if (isHealthy) {
+			isHealthy = false;
+			Debug.Log ("You got hit!");
+		} else {
+			Kill();
+		}
+	}
+
+	void ShootBullets () {
+		ammoCount = ammoCount > 0 ? ammoCount-1 : 0;
+	}
+
+	void Kill () {
+		isDead = true;
+		Debug.Log ("You would be dead now!");
+	}
+}
